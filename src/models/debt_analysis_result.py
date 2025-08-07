fbcf92f8a9f652ae9874c8734227d229f9f12db5
Andres Fernandez <andres.fernandez@cbqasolutions.com>
2025-08-06 22:28:02 -0600

    add: metric

--- src/models/debt_analysis_result.py
+++ src/models/debt_analysis_result.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 class DebtAnalysisResult(BaseModel):
     is_resolved: bool = Field(description="Describe si el cliente se comprometió a pagar la deuda o no")
+    is_satisfied: bool = Field(description="Describe si el cliente se mostró satisfecho con el trato del asistente")
+    promised_days: Optional[int] = Field(description="Número de días prometidos por el cliente para pagar la deuda. En caso de no especificar, se asumirá como indefinido.")
+    effective_contact: Optional[bool] = Field(description="Indica si el contacto con el cliente fue efectivo. Tiene que ser la misma persona a la que se busca")
